f48fb49b80bcc0c8999a3f7d83bf39661a8311d8
Illia Karpoff <hawaii2001@ukr.net>
2020-12-21 00:09:06 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import s from './App.module.css';
+import Profile from './components/Profile/Profile';
+import user from './data/user.json';
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './data/statistical-data.json';
+import FriendList from './components/FriendList/FriendList';
+import friends from './data/friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './data/transactions.json';
+
+function App() {
+  return (
+    <div className={s.container}>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics stats={statisticalData} />
+
+      <Statistics title="Upload stats with title" stats={statisticalData} />
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+    </div>
+  );
+}
+
+export default App;
